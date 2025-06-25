eae162e868fe4ae8ed0008a6684e2e487c514daf
YusranYuris <yusranyuris06@gmail.com>
2025-06-26 03:26:52 +0700

    Desain Detail Produk Jadi, belum integrasi DB

--- lib/penyewa_pages/detailProduk_page.dart
+++ lib/penyewa_pages/detailProduk_page.dart
@@ -18,72 +18,133 @@ class _DetailProdukPageState extends State<DetailProdukPage> {
     final Product currentProduct = widget.product;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(left: 6.0),
-          child: Text(
-            // Gunakan currentProduct.namaKendaraan dari widget
-            currentProduct.namaKendaraan,
-            style: const TextStyle(
-              fontFamily: 'Sora',
-              fontWeight: FontWeight.w700,
-              fontSize: 20,
-            ),
-          ),
-        ),
-        backgroundColor: const Color(0xFFE5ECF0),
-        // automaticallyImplyLeading: false, // Biasanya halaman detail ada tombol kembali, bisa diaktifkan
-      ),
+      extendBodyBehindAppBar: true,
       body: Container(
-        color: const Color(0xFFE5ECF0),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Detail Produk Page",
-                  style: TextStyle(
-                    fontFamily: 'Sora',
-                    fontSize: 18,
-                    color: Colors.black,
+        decoration: BoxDecoration(
+          color: Color(0xFFE5ECF0)
+        ),
+        child: SafeArea(
+          child: ListView(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ClipRRect(
+                        child: Image.asset(
+                          'assets/images/iqbal_rental.png',
+                          width: 406,
+                          height: 346,
+                        ),
+                      )
+                    ],
                   ),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  'Nama Rental: ${currentProduct.namaRental ?? 'Tidak Diketahui'}',
-                  style: const TextStyle(fontSize: 16, color: Colors.black),
-                ),
-                Text(
-                  'Lokasi Rental: ${currentProduct.lokasiRental ?? 'Tidak Diketahui'}',
-                  style: const TextStyle(fontSize: 16, color: Colors.black),
-                ),
-                Text(
-                  'Kendaraan: ${currentProduct.namaKendaraan}',
-                  style: const TextStyle(fontSize: 16, color: Colors.black),
-                ),
-                Text(
-                  'Harga: Rp${currentProduct.hargaProduk}/hari',
-                  style: const TextStyle(fontSize: 16, color: Colors.black),
-                ),
-                Text(
-                  'Deskripsi: ${currentProduct.deskripsiProduk}',
-                  style: const TextStyle(fontSize: 16, color: Colors.black),
-                ),
-                // Tambahkan gambar produk jika ada
-                if (currentProduct.gambarProduk != null) ...[
-                  const SizedBox(height: 20),
-                  Image.memory(
-                    currentProduct.gambarProduk!,
-                    width: 200,
-                    height: 200,
-                    fit: BoxFit.cover,
-                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24.0, right: 24.0),
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Rp275.000/hari",
+                            style: TextStyle(
+                              fontFamily: "Sora",
+                              fontWeight: FontWeight.w600,
+                              fontSize: 24,
+                              color: Colors.black
+                            ),
+                          ),
+                          Text(
+                            "Ducati Panigale V4",
+                            style: TextStyle(
+                              fontFamily: "Sora",
+                              fontWeight: FontWeight.w400,
+                              fontSize: 15,
+                              color: Colors.black
+                            ),
+                          ),
+                          Text(
+                            "Iqbal Rental",
+                            style: TextStyle(
+                              fontFamily: "Sora",
+                              fontWeight: FontWeight.w400,
+                              fontSize: 15,
+                              color: Colors.black
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Container(
+                            color: Colors.white,
+                            height: 2,
+                            width: double.infinity,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            "Deskripsi",
+                            style: TextStyle(
+                              fontFamily: "Sora",
+                              fontWeight: FontWeight.w700,
+                              fontSize: 18,
+                              color: Colors.black
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            "Masih Mulus, STNK Lengkap, Pajak Hidup, Ban Baru, Service Rutin, Siap Pakai.",
+                            style: TextStyle(
+                              fontFamily: "Sora",
+                              fontWeight: FontWeight.w400,
+                              fontSize: 15,
+                              color: Colors.black
+                            ),
+                          ),
+                          SizedBox(height: 250),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                width: 363,
+                                height: 56,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(4),
+                                  gradient: LinearGradient(
+                                    colors: [
+                                      Color(0xFF0F6D79),
+                                      Color(0xFF00BCD4)
+                                    ],
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight
+                                  ),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "Pesan Sekarang",
+                                      style: TextStyle(
+                                        fontFamily: "Sora",
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 20,
+                                        color: Colors.white
+                                      ), 
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  )
                 ],
-              ],
-            ),
-          ),
+              )
+            ],
+          )
         ),
       ),
     );
